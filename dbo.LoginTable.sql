1501d5067c4aade4cb24610bc7999e0e0bf42fb0
Matthmul <73168857+Matthmul@users.noreply.github.com>
2023-01-14 11:50:19 +0100

    Dodano baze danych

--- dbo.LoginTable.sql
+++ dbo.LoginTable.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[LoginTable]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [username] NVARCHAR(50) NULL,
-    [password] NVARCHAR(50) NULL
+    [password] NVARCHAR(50) NULL,
+    [isAdmin] BIT NOT NULL DEFAULT 0
 )
