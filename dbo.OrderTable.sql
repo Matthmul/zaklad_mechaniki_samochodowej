00eb63b18344f33d12c6c81523facadf6f36685f
Matthmul <73168857+Matthmul@users.noreply.github.com>
2023-01-18 01:44:02 +0100

    Dodano nowy interfejs dla Admina Zmiany w bazie danych w tabeli do zamowien Dodano Enum do organizacji postepu napraw

--- dbo.OrderTable.sql
+++ dbo.OrderTable.sql
@@ -1,8 +1,18 @@
 ﻿CREATE TABLE [dbo].[OrderTable]
 (
 	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
-	[ClientId] INT FOREIGN KEY REFERENCES LoginTable(Id),
-    [Brand] NVARCHAR (50) NOT NULL,
-    [Model] NVARCHAR (50) NOT NULL,
-    [Fix] BIT DEFAULT ((0)) NOT NULL
+	[ClientId] INT FOREIGN KEY REFERENCES LoginTable(Id) NOT NULL,
+    [Brand]                 NVARCHAR (50) NOT NULL,
+    [Model]                 NVARCHAR (50) NOT NULL,
+    [Fix]                   BIT           DEFAULT ((0)) NOT NULL,
+    [Review]                BIT           DEFAULT ((0)) NOT NULL,
+    [Assembly]              BIT           DEFAULT ((0)) NOT NULL,
+    [TechnicalConsultation] BIT           DEFAULT ((0)) NOT NULL,
+    [Training]              BIT           DEFAULT ((0)) NOT NULL,
+    [OrderingParts]         BIT           DEFAULT ((0)) NOT NULL,
+    [NrVIN]                 NVARCHAR (50) NOT NULL,
+    [ProductionYear]        INT NOT NULL,
+    [RegistrationNumber]    NVARCHAR (50) NOT NULL,
+    [EngineCapacity]        INT NOT NULL,
+    [OrderState]            NVARCHAR (50) NOT NULL,
 )
