2eee722d78b6d510772204ed0b6ad4f3b0b38538
Matthmul <73168857+Matthmul@users.noreply.github.com>
2023-01-25 00:20:14 +0100

    Dodano nowe dzialania do panelu obslugi uzytkownikow

--- ZakladMechanikiSamochodowej.Database/DatabaseActions/LoginTableActions.cs
+++ ZakladMechanikiSamochodowej.Database/DatabaseActions/LoginTableActions.cs
@@ -11,10 +11,10 @@ namespace ZakladMechanikiSamochodowej.Database.DatabaseActions
             database.SaveChanges();
         }
 
-        public static List<User> GetAllUsers()
+        public static List<User> GetAllAcceptedUsers()
         {
             using var dbContext = new DatabaseConnection();
-            return dbContext.LoginTable.ToList();
+            return dbContext.LoginTable.Where(u => u.IsNew == false).ToList();
         }
 
         public static User? TryGetUserByName(string name)
@@ -40,6 +40,26 @@ namespace ZakladMechanikiSamochodowej.Database.DatabaseActions
             using var dbContext = new DatabaseConnection();
             dbContext.LoginTable.Update(user);
             dbContext.SaveChanges();
+        }
+
+        public static void RemoveUser(User user)
+        {
+            using var dbContext = new DatabaseConnection();
+            dbContext.Remove(user);
+            dbContext.SaveChanges();
+        }
+
+        public static void AcceptUser(User user)
+        {
+            using var dbContext = new DatabaseConnection();
+            user.IsNew = false;
+            dbContext.Update(user);
+            dbContext.SaveChanges();
+        }
+
+        public static List<User> FindMatchingAcceptedUsers(string phrase)
+        {
+            return GetAllAcceptedUsers().Where(u => u.Username.Contains(phrase)).ToList();
         }
     }
 }
--- ZakladMechanikiSamochodowej/Admin/AccountsService.xaml.cs
+++ ZakladMechanikiSamochodowej/Admin/AccountsService.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using ZakladMechanikiSamochodowej.Database.DatabaseActions;
 using ZakladMechanikiSamochodowej.Database.DatabaseModels;
 
@@ -15,42 +17,97 @@ namespace ZakladMechanikiSamochodowej.Admin
         {
             InitializeComponent();
             LoadNewUsers();
+            LoadFindedUsers();
         }
 
         private void LoadNewUsers()
         {
-            var newUsersList = LoginTableActions.GetAllNewUsers();
+            listViewNewAccounts.ItemsSource = LoginTableActions.GetAllNewUsers();
+            CollectionView viewNew = (CollectionView)CollectionViewSource.GetDefaultView(listViewNewAccounts.ItemsSource);
+            viewNew.SortDescriptions.Clear();
+            viewNew.SortDescriptions.Add(new SortDescription("Username", ListSortDirection.Ascending));
+        }
 
-            foreach (var user in newUsersList)
-            {
-                ListViewItem lvi = new()
-                {
-                    Tag = user.Id,
-                    Content = user.Username
-                };
-                listViewNewAccounts.Items.Add(lvi);
-            }
+        private void LoadFindedUsers(List<User>? users = null)
+        {
+            listViewFoundAccounts.ItemsSource = (users != null) ? users : LoginTableActions.GetAllAcceptedUsers();
+            
+            CollectionView viewFound = (CollectionView)CollectionViewSource.GetDefaultView(listViewFoundAccounts.ItemsSource);
+            viewFound.SortDescriptions.Clear();
+            viewFound.SortDescriptions.Add(new SortDescription("Username", ListSortDirection.Ascending));
+        }
+
+        private void ListViewNewAccountsAscending_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listViewNewAccounts.ItemsSource);
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Username", ListSortDirection.Ascending));
+        }
+
+        private void ListViewNewAccountsDescending_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listViewNewAccounts.ItemsSource);
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Username", ListSortDirection.Descending));
+        }
+
+        private void ListViewFoundAccountsAscending_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listViewFoundAccounts.ItemsSource);
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Username", ListSortDirection.Ascending));
+        }
+
+        private void ListViewFoundAccountsDescending_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listViewFoundAccounts.ItemsSource);
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Username", ListSortDirection.Descending));
         }
 
         private void ButtonRefreshNewAccounts_Click(object sender, RoutedEventArgs e)
         {
-            listViewNewAccounts.Items.Clear();
             LoadNewUsers();
         }
 
         private void ButtonSearchAccount_Click(object sender, RoutedEventArgs e)
         {
-
+            List<User> listUsers = LoginTableActions.FindMatchingAcceptedUsers(searchPhrase.Text);
+            LoadFindedUsers(listUsers);
         }
 
-        private void ListViewFoundAccounts_DoubleClick(object sender, RoutedEventArgs e)
+        private void ButtonEditUser_Click(object sender, RoutedEventArgs e)
         {
-            
+            User user = (User)((Button)sender).DataContext;
+            AccountEdition accountEdition = new(user);
+            accountEdition.Show();
         }
 
-        private void ListViewNewAccounts_DoubleClick(object sender, RoutedEventArgs e)
+        private void ButtonDeleteUser_Click(object sender, RoutedEventArgs e)
         {
+            User user = (User)((Button)sender).DataContext;
+            if (user.Username == "admin")
+            {
+                MessageBox.Show(
+                    "Nie można usunąć konta administratora.",
+                    "Zakazana akcja",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+            LoginTableActions.RemoveUser(user);
 
+            LoadNewUsers();
+            LoadFindedUsers();
+        }
+
+        private void ButtonAcceptUser_Click(object sender, RoutedEventArgs e)
+        {
+            User user = (User)((Button)sender).DataContext;
+            LoginTableActions.AcceptUser(user);
+
+            LoadNewUsers();
+            LoadFindedUsers();
         }
     }
 }
